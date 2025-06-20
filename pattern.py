a39662ceddbc980b6ee3310f977e5eac9c3ddf7c
Suhani Mishra <suhanimishra613@gmail.com>
2025-06-21 00:04:35 +0530

    turtle learning in python

---
+++ pattern.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+#pentagon
+pencolor('red')
+speed('slowest')
+fillcolor('yellow')
+begin_fill()
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+end_fill()
+hideturtle()
+
+mainloop() #string ko rokne ke liye 
