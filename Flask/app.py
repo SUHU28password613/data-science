c3235f24224320c3ce26ff6b0dde30c2b37e1ca1
Suhani Mishra <suhanimishra613@gmail.com>
2025-07-11 18:04:39 +0530

    practisenumpy

--- Flask/app.py
+++ Flask/app.py
@@ -3,13 +3,32 @@ from flask import Flask, render_template
 app =  Flask(__name__)
 app.secret_key = 'supersecretmre'
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/servicepage')
+def servicepage():
+    return render_template('servicepage.html')
+
+@app.route('/doctors')
+def doctors():
+    return render_template('doctors.html')
+
+@app.route('/facilities')
+def facilities():
+    return render_template('facilities.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
 
 @app.route('/registerpage')
 def registerpage():
